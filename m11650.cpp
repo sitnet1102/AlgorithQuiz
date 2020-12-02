7cb8b83477167a4e4d3f87d2dd9c03bb99a1192a
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2020-12-02 22:32:45 +0900

    Update m11650.cpp

---
+++ m11650.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N;
+    vector<vector<int>> v;
+    vector<int> v2;
+    
+
+    return 0;
+}
