4dbe4a2832a2ba2dc6a366c81a25817b6722dce1
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2020-12-03 22:44:47 +0900

    Update m11650.cpp

--- m11650.cpp
+++ m11650.cpp
@@ -8,8 +8,18 @@ int main(){
     int N;
     vector<vector<int>> v;
     vector<int> v2;
+    int x,y;
     
-    scanf("%d",)
+    scanf("%d", &N);
+    for(int i=0;i<N;i++){
+        scanf("%d %d", &x, &y);
+        v2.push_back(x);
+        v2.push_back(y);
+        v.push_back(v2);
+        v2.empty();
+    }
+
+    
 
     return 0;
 }
