641faaf8fc13b0b1419c3b1b2a8d90569495d082
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2020-12-13 12:20:30 +0900

    20201213

--- m2580_1.cpp
+++ m2580_1.cpp
@@ -1,27 +1,19 @@
-/*
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0
-반례 
-*/
+// 스도쿠 채워질 때까지 돌아가는 방식으로 진행할 필요가 있음
+// 큐를 이용해서 넣고 빼는 것을 반복 
 
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int board[9][9];
 int v[81][2] = {{0,}};
+vector <pair <int, int>> v1;
 int M=0;
 
 bool possible(int x, int y){
-    int check[10]={0,};
+    //int check[10]={0,};
     int width = 0;  // 가로
     int depth = 0;  // 세로
     int sqaure = 0;
@@ -41,7 +33,7 @@ bool possible(int x, int y){
     // 사각형 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(board[x3 * 3 + j+1][y3 * 3 + i+1] == board[x][y]) {
+            if(board[x3 * 3 + j][y3 * 3 + i] == board[x][y]) {
                 sqaure ++;
             }
         }
@@ -55,13 +47,16 @@ bool possible(int x, int y){
 }
 
 void func(int k){
-    if(k == M){
+    //if(k == M){
+    if(sizeof(v1) == 0){
         //int Ans=0;
+        /*
         for(int i=0;i<M;i++){
             if(board[v[i][0]][v[i][1]] == 0){
                 return;
             }
         }
+        */
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 cout << board[j][i];
@@ -71,15 +66,15 @@ void func(int k){
             }
             cout << '\n';            
         }
-        exit(0);
+        //exit(0);
         return;
     }
 
     for(int i=1;i<=9;i++){
-        board[v[k][0]][v[k][1]] = i;
-        if(possible(v[k][0], v[k][1])){
+        board[v1[k].first][v1[k].second] = i;
+        if(possible(v1[k].first, v1[k].second)){
             func(k+1);
-            board[v[k][0]][v[k][1]] = 0;
+            board[v1[k].first][v1[k].second] = 0;
         }
     }
 }
@@ -89,9 +84,15 @@ int main(){
         for(int j=0;j<9;j++){
             cin >> board[j][i];
             if(!board[j][i]){
+                pair<int, int> tmp;
+                tmp.first = j;
+                tmp.second = i;
+                v1.push_back(tmp);
+                /*
                 v[M][0] = j;
                 v[M][1] = i;
                 M++;
+                */
             }
         }
     }
