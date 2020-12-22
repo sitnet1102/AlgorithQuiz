d1902ce88245d79199818679471383ab2bbe7210
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2020-12-23 00:06:18 +0900

    Create m14888.cpp

---
+++ m14888.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+int A[4];
+int max, min;
+vector<int> v;
+
+void func(int k){
+    if(k == N){
+        for(int i=0;i<N;i++){
+            cout << v[i] << ' ';
+        }
+        cout <<'\n';
+    }
+    for(int i=1;i<=N;i++){
+        if(!isused[i] && (i > v[k-1] || k == 0)){
+            v[k] = i;
+            isused[i] = 1;
+            func(k+1);
+            isused[i] = 0;
+        }
+    }
+}
+
+
+int main(){
+    cin >> N;
+    for(int i=0;i<N;i++){
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    for(int i=0;i<4;i++){
+        cin >> A[i];
+    }
+
+    func(0);
+    return 0;
+}
