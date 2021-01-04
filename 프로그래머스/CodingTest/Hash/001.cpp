27041bfa61fbc1cc0595f2532509bd8fcd94c5a9
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-01-04 18:01:41 +0900

    20210104
    프로그래머스
    코딩테스트 해시

--- 프로그래머스/CodingTest/Hash/001.cpp
+++ 프로그래머스/CodingTest/Hash/001.cpp
@@ -1,9 +1,49 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
+    int num = participant.size();
+    bool check = true;
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    for(int i=0;i<num-1;i++){
+        if(participant[i] != completion[i]){
+            answer = participant[i];
+            check = false;
+            break;
+        }
+    }
+    /*
+    cout << "p = ";
+    for(int i=0;i<num;i++){
+        cout << participant[i] << " ";
+    }
+    cout << "\n";
+    cout << "c = ";
+    for(int i=0;i<num-1;i++){
+        cout << completion[i] << " ";
+    }
+    cout << "\n";
+    */
+    /*
+    for(int i=0;i<num;i++){
+        for(int j=0;j<completion.size();j++){
+            if(participant[0] == completion[j]){
+                participant.erase(participant.begin());
+                completion.erase(completion.begin() + j);
+                break;
+            }
+        }
+    }
+    */
+   if(check){
+       answer = participant[num-1];
+   }
+    //answer = participant[0];
     return answer;
 }
---
+++ 프로그래머스/CodingTest/Hash/003.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool comp(vector<string> a, vector<string> b){
+    if(a[1] == b[1]){
+        return a[0] < b[0];
+    }
+    return a[1] < b[1];
+}
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    int num=0;
+    int r_num=1;
+    vector<int> v;
+    sort(clothes.begin(), clothes.end(), comp);
+    cout << clothes.size() << "\n";
+    if(clothes.size() == 1){
+        return 1;
+    }
+
+    for(int i=0;i<clothes.size();i++){
+        num++;
+        if(i==clothes.size()-1){
+            v.push_back(num);
+        }else{
+            if(clothes[i][1] != clothes[i+1][1]){
+                v.push_back(num);
+                num = 0;
+            }
+        }
+    }
+    for(int i=0;i<v.size();i++){
+        cout << v[i] << " ";
+        r_num = r_num * (v[i]+1);
+    }
+    cout << "\n";
+    
+    for(int i=0;i<clothes.size();i++){
+        cout << clothes[i][0] << " " << clothes[i][1] << "\n";
+    }
+    
+    
+    answer = r_num - 1;
+    return answer ;
+}
+
+/*
+// 63
+[
+    ["a","aa"],
+    ["b","aa"],
+    ["c","aa"],
+    ["a_a","bb"],
+    ["b_b","bb"],
+    ["c_c","bb"],
+    ["aaa","cc"],
+    ["bbb","cc"],
+    ["ccc","cc"]
+
+]
+
+
+// 7
+[
+    ["a", "a"], 
+    ["b", "b"], 
+    ["c", "c"]
+]
+
+*/
