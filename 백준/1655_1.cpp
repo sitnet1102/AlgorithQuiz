aae805e9464d3e89e6f7e2889053f11aca734311
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-01-13 11:13:30 +0900

    20210113
    백준 우선순위큐

--- 백준/1655_1.cpp
+++ 백준/1655_1.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 int main () {
     int N;
-    vector<int> v;
     vector<int> ans;
     priority_queue<int, vector<int>, less<int>> max;    // 작은쪽
     priority_queue<int, vector<int>, greater<int>> min; // 큰쪽
@@ -17,13 +16,43 @@ int main () {
     for(int i=0;i<N;i++){
         int tmp;
         cin >> tmp;
-        v.push_back(tmp);
-        sort(v.begin(), v.end());
-        ans.push_back(v[(v.size()-1)/2]);
+        if(max.empty()){
+            max.push(tmp);
+        }else if(min.empty()){
+            if(max.top() > tmp){
+                min.push(max.top());
+                max.pop();
+                max.push(tmp);
+            }else{
+                min.push(tmp);
+            }
+        }else{
+            min.push(tmp);
+            int a = max.top();
+            int b = min.top();
+            if(min.size() > max.size()){
+                min.pop();
+                max.push(b);
+            }else if(a>b){
+                max.pop();
+                min.pop();
+                max.push(b);
+                min.push(a);
+            }
+        }
+        ans.push_back(max.top());
+        /*   
+        cout << "==============================\n";
+        cout << max.size() << " " << min.size() << "\n";
+        for(int j=0;j<ans.size();j++){
+            cout << ans[j] << " ";
+        }
+        cout << "\n";
+        */
     }
-
     for(int i=0;i<ans.size();i++){
         cout << ans[i] << "\n";
     }
+
     return 0;
 }
