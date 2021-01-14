1cc430a74aaeab3ff046b8d812c155a485c45a49
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-01-14 16:40:38 +0900

    20210114
    프로그래머스
    코딩테스트 연습
    깊이/너비 우선 탐색

---
+++ 프로그래머스/CodingTest/DFS,BFS/001.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> isused;
+vector<int> num;
+int n;
+int t;
+int ans = 0;
+
+void func(int k){
+
+    if(k==n){
+        int sum = 0;
+        for(int i=0;i<isused.size();i++){
+            if(isused[i] == 1){
+                sum = sum + num[i];
+            }else if(isused[i] == 2){
+                sum = sum - num[i];
+            }
+        }
+        if(sum == t){
+            ans ++;
+        }
+        return ;
+    }
+
+    for(int i=0;i<n;i++){
+        if(!isused[i]){
+            isused[i] = 1;  // 더하기 
+            func(k+1);
+            isused[i] = 2;  // 빼기 
+            func(k+1);
+            isused[i] = 0;
+        }
+    }
+
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    isused.clear();
+    for(int i=0;numbers.size();i++){
+        isused.push_back(0);
+    }
+    num.clear();
+    for(int i=0;i<numbers.size();i++){
+        num.push_back(numbers[i]);
+    }
+    n = numbers.size();
+    t = target;
+    func(0);
+    return answer;
+}
---
+++ 프로그래머스/CodingTest/DFS,BFS/001_1.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    int sum = 0;
+    for(int i=0;i<numbers.size();i++){
+        if
+    }
+
+    return answer;
+}
