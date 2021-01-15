d7e7a1de26a2f135d37be0bb9db41c41d5f54388
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-01-15 14:43:18 +0900

    20210115
    프로그래머스
    코딩 테스트 연습
    깊이/너비 우선 탐색

--- 프로그래머스/CodingTest/DFS,BFS/001_1.cpp
+++ 프로그래머스/CodingTest/DFS,BFS/001_1.cpp
@@ -5,12 +5,46 @@
 
 using namespace std;
 
-int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    int sum = 0;
-    for(int i=0;i<numbers.size();i++){
-        for(int j=0;j<)
+int isused[20];
+int num[20];
+int n;
+int t;
+int s;
+int ans = 0;
+
+void func(int k, int a){
+    if(k==a){
+        int sum = s;
+        for(int i=0;i<n;i++){
+            if(isused[i] == 1){
+                sum = sum - num[i];
+            }
+        }
+        if(sum == t){
+            ans ++;
+        }
+        return ;
+    }
+    for(int i=0;i<n;i++){
+        if(!isused[i]){
+            isused[i] = 1;
+            func(k+1);
+            isused[i] = 0;
+        }
     }
 
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    for(int i=0;i<numbers.size();i++){
+        num[i] = numbers[i];
+    }
+    n = numbers.size();
+    t = target;
+    for(int i=1;i<n;i++){
+        func(0,i);
+    }
+    answer = ans;
     return answer;
 }
