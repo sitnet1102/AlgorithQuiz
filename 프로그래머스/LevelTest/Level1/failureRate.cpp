42b7e29ff4968512e3e1221baae22b9128ddd9d1
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-01-22 18:32:27 +0900

    20210122
    프로그래머스
    레벨 테스트
    1레벨

--- 프로그래머스/LevelTest/Level1/failureRate.cpp
+++ 프로그래머스/LevelTest/Level1/failureRate.cpp
@@ -1,19 +1,46 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
+bool cmp(pair<int, double> a, pair<int, double> b){
+    if(a.second == b.second){
+        return a.first < b.first;
+    }
+    return a.second > b.second;
+}
+
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
-    vector<int> v;
+    vector<pair<int,double>> v;
     sort(stages.begin(), stages.end());
     for(int i=1;i<=N;i++){
-        int m;
+        pair<int, double> p;
+        int notuser = 0;
+        int fail_person = 0;
+        p.first = i;
         for(int j=0;j<stages.size();j++){
-            m = j+1;
-            if()
+            if(stages[j] == i){
+                fail_person++;
+            }else if(stages[j] > i){
+                break;
+            }else{
+                notuser ++;
+            }
         }
+        if(notuser == stages.size()){
+            p.second = 0;
+        }else{
+            p.second = (float)fail_person / (float)(stages.size() - notuser);
+        }
+        v.push_back(p);
+    }
+    sort(v.begin(), v.end(), cmp);
+    for(int i=0;i<v.size();i++){
+        //cout << v[i].first << " " << v[i].second <<"\n"; 
+        answer.push_back(v[i].first);
     }
     return answer;
 }
