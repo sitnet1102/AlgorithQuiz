243b4efdb96a4d04b2f2debc1d86c08a25570901
sitnet1102 <43734934+sitnet1102@users.noreply.github.com>
2021-01-25 18:39:42 +0900

    20210125
    프로그래머스
    코딩 테스트 연습
    레벨 2

--- 프로그래머스/LevelTest/Level2/skillTree.cpp
+++ 프로그래머스/LevelTest/Level2/skillTree.cpp
@@ -12,14 +12,29 @@ int solution(string skill, vector<string> skill_trees) {
     }
     for(int i=0;i<skill_trees.size();i++){
         int num = 0;
+        int num2 = 0;
+        vector<int> v;
+        for(int j=0;j<s.size();j++){
+            v.push_back(0);
+        }
         for(int j=0;j<skill_trees[i].size();j++){
+            for(int k=0;k<s.size();k++){
+                if(skill_trees[i].substr(j,1) == s[k]){
+                    v[k] = 1;
+                }
+            }
             if(s[num] == skill_trees[i].substr(j,1)){
                 num ++;
             }
         }
-        if(num == skill.size()){
+        for(int j=0;j<v.size();j++){
+            if(v[j]==1){
+                num2++;
+            }
+        }
+        if(num == num2){
             answer++;
-            cout << skill_trees[i] << "\n";
+            //cout << skill_trees[i] << "\n";
         }
     }
     return answer;
